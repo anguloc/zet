c8d2cb3bdfee3a5602787554f45b6a2c3eb6a82e
gk <xy62201793@163.com>
2023-10-06 05:47:18 +0800

    tmp

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,33 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
-	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
-	url := "https://www.bitget.com/v1/finance/trend/product/list"
+	fmt.Println("start")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadBytes(byte('\n'))
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep(200 * time.Millisecond)
+				continue
+			} else {
+				break
+			}
+		}
+		if !json.Valid(line) {
+			//fmt.Println("no_json")
+		}
+		//fmt.Println(string(line))
+	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
-	//req.Header.Set("Cookie", "cf_chl_2=e0348379c623e39; cf_clearance=_PX2oM8ikiRzHxjvdXwA.AwijXomPo8oViMMEwoXk3E-1695459500-0-1-1e08f744.4e3d7682.4909a8a4-150.0.0; __cf_bm=JP3D1ifHWq3n9aib6M6du8YgrKTmH4fEyySlguh8Rxo-1695459503-0-Ab/BGH02CTGLxmftRYNZ/MtpPfi49F9+NYzhwF25E5X0ZOWpNz9UUKS0gX4S2oz3biTOionPsJ9YIzKnJiKwA1E=; _cfuvid=O8yulUvZrfUWBlyvo_3tFPCDYCb8DIdovN.lTG8T.jo-1695459503118-0-604800000")
+	fmt.Println("end")
+}
 
-	client := &http.Client{}
-	rsp, err := client.Do(req)
-	if err != nil {
-		println(123)
-		panic(err)
-	}
-	defer rsp.Body.Close()
-	data, err := io.ReadAll(rsp.Body)
-	if err != nil {
-		println(456)
-		panic(err)
-	}
-	fmt.Println(string(data))
-	println("httpcode:", rsp.StatusCode)
+func r(ctx context.Context) {
+	wg := &sync.WaitGroup{}
+	sig := make(chan os.Signal, 2)
+	signal.Notify(sig, []os.Signal{syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM}...)
+	go func() {
+		s := <-sig
+		go func() {
+			_ = app.stopWorker(ctx, s != syscall.SIGQUIT)
+		}()
+		<-sig
+		os.Exit(128 + int(s.(syscall.Signal)))
+	}()
+	wg.Wait()
 }
