a337e9b733c1d2bed9749fa00246da7212fd1c41
gk <xy62201793@163.com>
2023-10-18 23:28:09 +0800

    tmp

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -4,15 +4,40 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/anguloc/zet/pkg/application"
 	"github.com/anguloc/zet/pkg/console"
+	"golang.org/x/sync/semaphore"
 )
 
 func main() {
-	console.SetLevel(console.DebugLevel, console.InfoLevel, console.WarnLevel, console.ErrorLevel)
 	ctx := context.TODO()
+
+	wg := &sync.WaitGroup{}
+	t := semaphore.NewWeighted(3)
+
+	list := getList()
+
+	for _, s := range list {
+		wg.Add(1)
+		if aerr := t.Acquire(ctx, 1); aerr != nil {
+			wg.Done()
+			continue
+		}
+		go func(s string) {
+			defer wg.Done()
+			defer t.Release(1)
+			httpRes := mockHttp(s)
+			fmt.Printf("%s,res:%s\n", time.Now().Format(time.RFC3339), httpRes)
+		}(s)
+	}
+
+	wg.Wait()
+
+	return
+	console.SetLevel(console.DebugLevel, console.InfoLevel, console.WarnLevel, console.ErrorLevel)
 	app := application.New()
 
 	app.RegisterWorker("script", application.NewScript(handle))
@@ -25,10 +50,37 @@ func main() {
 	}
 }
 
+func getList() []string {
+	res := make([]string, 0, 10000)
+	for i := 0; i < 8000; i++ {
+		res = append(res, "zcasd")
+	}
+	return res
+}
+
+func mockHttp(s string) string {
+	time.Sleep(1 * time.Second)
+	return "asd"
+}
+
 func handle(ctx context.Context, param *application.ScriptParam) {
 	for param.IsRun() {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Println(param.IsRun())
 	}
 	time.Sleep(100 * time.Millisecond)
+}
+
+func touchPanic() {
+	var err error
+	res, _ := func() (string, error) {
+		defer func() {
+			if perr := recover(); perr != nil {
+				err = fmt.Errorf("asdasdasd,%+v", perr)
+			}
+		}()
+		panic("asdasda")
+	}()
+	_ = res
+	fmt.Println(err)
 }
